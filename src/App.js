7018e550c0f2ca957259c7d9983d57f7fc0c5228
AMAL RAJ T <amal.art.art@gmail.com>
2023-09-13 12:08:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// src/components/ImageGallery.js
+import React, { useEffect, useState } from "react";
+import NavBar from "./components/NavBar.js";
 
-function App() {
+function ImageGallery() {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchImages = async (pageNumber) => {
+    try {
+      const response = await fetch(
+        `https://picsum.photos/v2/list?page=${pageNumber}&limit=10`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setImages([...images, ...data]);
+      } else {
+        console.error("Error fetching images:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching images:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages(page);
+  }, [page]);
+
+  const loadMoreImages = () => {
+    setPage(page + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="min-h-screen flex flex-col items-center justify-center p-9 bg-black">
+      <NavBar />
+      <div className="w-full h-full flex flex-col items-center justify-center p-16">
+        <img src="/header.jpg" className="w-[80%] h-[10%] -mt-10 mb-8 rounded-lg shadow-md" alt="" />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          {images.map((image) => (
+            <div
+              key={image.id}
+              className="bg-white rounded-lg overflow-hidden shadow-md"
+            >
+              <img src={image.download_url} alt={image.author} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mt-4 text-center">
+        <button
+          onClick={loadMoreImages}
+          className="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 cursor-pointer"
         >
-          Learn React
-        </a>
-      </header>
+          Load More
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default ImageGallery;
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {FaRegImage} from "react-icons/fa"
+
+function NavBar() {
+  return (
+    <div className="w-[95%] min-h-[20%] flex flex-row items-center justify-center mb-5  bg-black ">
+    <img src="/1.webp" className="w-[4%] h-[20%] flex flex-row items-center justify-center" alt="" />
+    <h1 className="text-[35px] w-[90%] text-white font-bold flex flex-row items-center justify-center">PixelPioneers</h1>
+    </div>
+  );
+}
+
+export default NavBar;
